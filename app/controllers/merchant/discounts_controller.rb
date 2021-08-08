6950965956ab8c3f3a8c372b4a98ae78abcc3c2c
Taylor Varoglu <tava8398@colorado.edu>
2021-08-08 16:07:18 -0600

    Add discount#update to controllers, views, and spec

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -14,19 +14,33 @@ class Merchant::DiscountsController < ApplicationController
   end
 
   def edit
+    @discount = Discount.find(params[:id])
   end
 
   def create
+    @discount = Discount.new(discount_params)
+    if @discount.save
+      redirect_to merchant_discounts_path(params[:merchant_id])
+      flash[:notice] = "Bulk discount successfully created."
+    else
+      redirect_to new_merchant_discount_path(params[:merchant_id])
+      flash[:alert] = "All fields are required."
+    end
   end
 
   def update
-    # @merchant = Merchant.find(params[:merchant_id])
-    # @invoice = Invoice.find(params[:id])
-    # @invoice_item = InvoiceItem.find(params[:ii_id])
-    # @invoice_item.update(status: params[:status])
-    # @invoice_item.save
-    # redirect_to "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
-    # flash[:notice] = "Item status successfully updated!"
+    discount = Discount.find(params[:id])
+    discount.update(discount_model_params)
+    redirect_to merchant_discount_path(discount.merchant_id, discount.id), notice: "Bulk discount successfully updated."
+  end
+
+  private
+  def discount_params
+    params.permit(:quantity_threshold, :percentage_discount, :status, :merchant_id)
+  end
+
+  def discount_model_params
+    params.require(:discount).permit(:quantity_threshold, :percentage_discount, :status)
   end
 
 end
