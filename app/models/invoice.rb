3fb4f84425c6ee586256d2747248e8e7a5fc7230
Taylor Varoglu <tava8398@colorado.edu>
2021-08-09 16:34:34 -0600

    Add Invoice.discounted_revenue_for_merchant to models and spec

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -12,4 +12,35 @@ class Invoice < ApplicationRecord
     invoice_items.sum("invoice_items.unit_price * invoice_items.quantity")
   end
 
+  def invoice_item_totals(merchant_id = nil)
+    query = invoice_items.select("invoice_items.id, invoice_items.quantity,
+      SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue_potential")
+    .joins(item: :merchant)
+    .group("invoice_items.id")
+    merchant_id == nil ? query : query.where("items.merchant_id = ?", merchant_id)
+  end
+
+  def applicable_discount_for_merchant(merchant)
+    invoice_item_totals(merchant.id).each do |invoice_item|
+      merchant.discounts_ordered_by_percentage_discount.each do |discount|
+        return discount if invoice_item.quantity >= discount.quantity_threshold
+      end
+    end
+  end
+
+  def best_discount_for_merchant(merchant)
+    merchant.has_multiple_discounts? ? applicable_discount_for_merchant(merchant): merchant.best_discount
+  end
+
+  def discounted_revenue_for_merchant(merchant)
+    best_discount = best_discount_for_merchant(merchant)
+    invoice_item_totals(merchant.id).reduce(0) do |total, invoice_item|
+      if invoice_item.quantity >= best_discount.quantity_threshold
+        total + (invoice_item.revenue_potential * (1 - best_discount.percentage_discount))
+      else
+        total + invoice_item.revenue_potential
+      end
+    end.round(2)
+  end
+
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -17,30 +17,53 @@ RSpec.describe Discount do
 
   describe 'methods' do
     before :each do
-      @merchant = Merchant.create!(name: 'Tom Holland', status: 0)
-      @discount = @merchant.discounts.create!(quantity_threshold: 5, percentage_discount: 0.5, status: 0)
+      @merchant_1 = Merchant.create!(name: 'Tom Holland', status: 0)
+      @discount_1 = @merchant_1.discounts.create!(quantity_threshold: 1, percentage_discount: 0.25, status: 0)
+      @discount_2 = @merchant_1.discounts.create!(quantity_threshold: 5, percentage_discount: 0.5, status: 0)
+
+      @merchant_2 = Merchant.create!(name: 'Borkanth')
 
       @customer_1 = Customer.create!(first_name: 'Green', last_name: 'Goblin')
 
       @invoice_1 = Invoice.create!(status: 2, customer_id: @customer_1.id)
 
-      @item_1 = Item.create!(name: 'spider suit', description: 'saves lives', unit_price: '10000', merchant_id: @merchant.id)
-      @item_2 = Item.create!(name: 'web shooter', description: 'shoots webs', unit_price: '5000', merchant_id: @merchant.id)
-      @item_3 = Item.create!(name: 'upside down kiss', description: 'That Mary jane Swag', unit_price: '15000', merchant_id: @merchant.id)
+      @item_1 = Item.create!(name: 'spider suit', description: 'saves lives', unit_price: '10000', merchant_id: @merchant_1.id)
+      @item_2 = Item.create!(name: 'web shooter', description: 'shoots webs', unit_price: '5000', merchant_id: @merchant_1.id)
+      @item_3 = Item.create!(name: 'upside down kiss', description: 'That Mary jane Swag', unit_price: '15000', merchant_id: @merchant_1.id)
 
       @inv_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 2, unit_price: 200, status: 1)
     end
 
     describe '#status_opposite' do
       it "returns the opposite of the discount's enabled/disabled status" do
-        expect(@discount.status).to eq('enabled')
-        expect(@discount.status_opposite).to eq('disabled')
+        expect(@discount_1.status).to eq('enabled')
+        expect(@discount_1.status_opposite).to eq('disabled')
       end
     end
 
     describe '#formatted_percentage' do
       it "returns the formatted percentage of the discount's percentage discount table column" do
-        expect(@discount.formatted_percentage).to eq("#{(@discount.percentage_discount * 100).round}%")
+        expect(@discount_1.formatted_percentage).to eq("#{(@discount_1.percentage_discount * 100).round}%")
+      end
+    end
+
+    describe '#has_multiple_discounts?' do
+      it "returns a boolean to signify whether a merchant has multiple discounts" do
+        expect(@merchant_1.has_multiple_discounts?).to be true
+        expect(@merchant_2.has_multiple_discounts?).to be false
+      end
+    end
+
+    describe '#discounts_ordered_by_percentage_discount' do
+      it "returns all discounts in descending order of percentage_discount when a merchant has multiple discounts" do
+        expect(@merchant_1.discounts_ordered_by_percentage_discount).to eq([@discount_2, @discount_1])
+      end
+    end
+
+    describe '#best_discount' do
+      it "returns the best discount for a merchant based on the percentage_discount column" do
+        expect(@merchant_1.best_discount).to eq(@discount_2)
+        expect(@merchant_2.best_discount).to eq(nil)
       end
     end
   end
